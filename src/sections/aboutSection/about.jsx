2bcca94dcb2f288fe937f0ce9d5d4e9975be0207
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-12-15 10:11:22 +0200

    about almost done

--- src/sections/aboutSection/about.jsx
+++ src/sections/aboutSection/about.jsx
@@ -5,11 +5,17 @@ import Image1 from '/public/images/img1.jpg';
 export default function About() {
     return (<>
         <div className="about">
-            <div className="about_text">
-                <h1>hello from about</h1>
+            <div className="about-text">
+                <div className="about-headings">
+                    <h4>welcome to</h4>
+                    <h1>business name here</h1>
+                    <h2>business model here</h2>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est qui, consequuntur officiis iure asperiores ex ab placeat itaque architecto molestiae dolor amet, saepe quidem quis numquam provident veniam corporis. Eligendi.</p>
+
             </div>
-            <div className="about_image">
-                <Image src={Image1} width={600} height={600} loading={"lazy"} placeholder={"blur"}/>
+            <div className="about-image">
+                <Image src={Image1} alt="about image" loading={"lazy"} placeholder={"blur"} />
             </div>
         </div>
     </>)
